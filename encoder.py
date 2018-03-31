a4d6d500201ee6ddac3ebec8bf6bc69959c15917
Sameer Jain <jain.sameer96@gmail.com>
2018-03-31 23:20:27 +0530

    Update encoder.py
    Converted from character-based to word-based model

--- encoder.py
+++ encoder.py
@@ -2,16 +2,19 @@ from keras.models import Model
 from keras.layers import Input, LSTM, Dense, GRU
 from keras.optimizers import Adam
 
+#set num_encoder_tokens and num_decoder_tokens equal to the corpus vocabulary
 
-encoder_inputs = Input(shape=(None, num_encoder_tokens))
+encoder_inputs = Input(shape=(None,))
+x = Embedding(num_encoder_tokens, latent_dim)(encoder_inputs)
 encoder = GRU(latent_dim, return_state=True, recurrent_initializer='orthogonal')
-encoder_outputs, state_h = encoder(encoder_inputs)
+encoder_outputs, state_h = encoder(x)
 
-decoder_inputs = Input(shape=(None, num_decoder_tokens))
-decoder_gru = GRU(latent_dim, return_sequences=True)
-decoder_outputs = decoder_gru(decoder_inputs, initial_state=state_h)
+decoder_inputs = Input(shape=(None,))
+x = Embedding(num_decoder_tokens, latent_dim)(decoder_inputs)
+decoder_gru = GRU(latent_dim, return_sequences=True)(x, initial_state=state_h)
 decoder_dense = Dense(num_decoder_tokens, activation='softmax')
-decoder_outputs = decoder_dense(decoder_outputs)
+decoder_outputs = decoder_dense(decoder_gru)
+
 model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 
