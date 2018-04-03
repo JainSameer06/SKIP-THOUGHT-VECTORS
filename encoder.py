b702b1ffbbfb23d2ac2e4636b4b0320372933f9e
Sameer Jain <jain.sameer96@gmail.com>
2018-04-03 21:46:04 +0530

    Update encoder.py
    added preprocessing steps

--- encoder.py
+++ encoder.py
@@ -1,10 +1,22 @@
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.text import pad_sequences
+from keras.models import Sequential
+from keras.layers import Flatten
+from keras.layers.embeddings import Embedding
 from keras.models import Model
 from keras.layers import Input, LSTM, Dense, GRU
 from keras.optimizers import Adam
 
-#set num_encoder_tokens and num_decoder_tokens equal to the corpus vocabulary
+vocab_size = 100000 # set according to previous step
+latent_dim = 2400
+
+encoded = [one_hot(d, vocab_size) for d in docs] # docs is the list of sentences from the corpus where each sentence is a string
+max_sentence_length = 20 # set according to previous step
+padded = pad_sequences(encoded, maxlen = max_sentence_length, padding = 'post')
+#feed sentences from padded into the encoder, and the next sentences, into the decoder 
 
 encoder_inputs = Input(shape=(None,))
+
 x = Embedding(num_encoder_tokens, latent_dim)(encoder_inputs)
 encoder = GRU(latent_dim, return_state=True, recurrent_initializer='orthogonal')
 encoder_outputs, state_h = encoder(x)
@@ -20,4 +32,3 @@ model = Model([encoder_inputs, decoder_inputs], decoder_outputs)
 
 adam = Adam(lr=0.01, decay=1e-6,beta_1 =0.9 , beta_2 = 0.999, clipnorm=10)
 model.compile(loss='mean_squared_error', optimizer=adam)
-
